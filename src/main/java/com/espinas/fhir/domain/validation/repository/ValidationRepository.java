4f330d7869e554f2b5f855560d24d8c526cde3b2
jhkang <jhkang1517@gmail.com>
2022-07-04 10:50:36 +0900

    config : Configuration MongoDB
    Signed-off-by: jhkang <jhkang1517@gmail.com>

--- src/main/java/com/espinas/fhir/domain/validation/repository/ValidationRepository.java
+++ src/main/java/com/espinas/fhir/domain/validation/repository/ValidationRepository.java
@@ -2,6 +2,7 @@ package com.espinas.fhir.domain.validation.repository;
 
 import com.espinas.fhir.domain.validation.collection.Validation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 public interface ValidationRepository extends MongoRepository<Validation, String> {
 }
