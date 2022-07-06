692d96fff4c298a4213a73c1e7d4eb170b26c9b3
jhkang1517 <jhkang1517@gmail.com>
2022-07-06 23:59:20 +0900

    refactor : Code refactoring
    Signed-off-by: jhkang1517 <jhkang1517@gmail.com>

--- src/main/java/com/espinas/fhir/domain/validation/repository/ValidationRepository.java
+++ src/main/java/com/espinas/fhir/domain/validation/repository/ValidationRepository.java
@@ -2,7 +2,6 @@ package com.espinas.fhir.domain.validation.repository;
 
 import com.espinas.fhir.domain.validation.collection.Validation;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 public interface ValidationRepository extends MongoRepository<Validation, String> {
 }
