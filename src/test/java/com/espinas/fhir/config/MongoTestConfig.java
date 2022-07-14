92af22faa9cfda1b529ed01edd091c641227f921
jhkang <jhkang1517@gmail.com>
2022-07-14 13:26:40 +0900

    hotfix : Hotfix EnableMongoRepositories configuration
    Signed-off-by: jhkang <jhkang1517@gmail.com>

--- src/test/java/com/espinas/fhir/config/MongoTestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.espinas.fhir.config;
-
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-
-@TestConfiguration
-@EnableMongoAuditing
-public class MongoTestConfig {
-}
--- src/test/java/com/espinas/fhir/support/RepositoryTestSupport.java
+++ src/test/java/com/espinas/fhir/support/RepositoryTestSupport.java
@@ -1,10 +1,8 @@
 package com.espinas.fhir.support;
 
-import com.espinas.fhir.config.MongoTestConfig;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -12,6 +10,5 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @DataMongoTest
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@Import(MongoTestConfig.class)
 public class RepositoryTestSupport {
 }
