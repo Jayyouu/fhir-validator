70a8380e92a8829268c4e1e8d0401b49b6361afc
jhkang <jhkang1517@gmail.com>
2022-07-14 13:33:40 +0900

    config : Remove useless import package
    Signed-off-by: jhkang <jhkang1517@gmail.com>

--- src/main/java/com/espinas/fhir/Application.java
+++ src/main/java/com/espinas/fhir/Application.java
@@ -3,7 +3,6 @@ package com.espinas.fhir;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableConfigurationProperties(value = {ApplicationProperties.class})
 @SpringBootApplication
