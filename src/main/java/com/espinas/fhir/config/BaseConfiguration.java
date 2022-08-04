3c0927fc23fd513eaa361c95f4fe97ff2c0f8688
jhkang <jhkang1517@gmail.com>
2022-08-04 14:33:36 +0900

    feat : Create basePackagePath
    Signed-off-by: jhkang <jhkang1517@gmail.com>

---
+++ src/main/java/com/espinas/fhir/config/BaseConfiguration.java
@@ -0,0 +1,5 @@
+package com.espinas.fhir.config;
+
+public class BaseConfiguration {
+    public static final String basePackagePath = "com.espinas.fhir";
+}
--- src/main/java/com/espinas/fhir/config/MongoConfiguration.java
+++ src/main/java/com/espinas/fhir/config/MongoConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories(basePackages = {"com.espinas.fhir.*"})
+@EnableMongoRepositories(basePackages = {BaseConfiguration.basePackagePath + ".*"})
 @EnableMongoAuditing
 public class MongoConfiguration extends AbstractMongoClientConfiguration {
 
